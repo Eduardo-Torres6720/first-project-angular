5fb868a8294d66943d1ebd39d991acc0ad11de32
Eduardo-Torres6720 <eduardotorres6720@gmail.com>
2024-08-03 15:37:52 -0300

    feat: defining routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './pages/login/login.component';
+import { MainComponent } from './pages/shared/main/main.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'login', component: LoginComponent },
+  {
+    path: '',
+    component: MainComponent,
+    children: [{ path: '', component: HomeComponent }],
+  },
+];
