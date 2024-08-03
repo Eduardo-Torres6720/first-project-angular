28956adbe108505a04b2a68bc8e9b49a21e61ade
Eduardo-Torres6720 <eduardotorres6720@gmail.com>
2024-08-03 17:41:20 -0300

    feat: first version main component

--- src/app/pages/shared/main/main.component.ts
+++ src/app/pages/shared/main/main.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { MatIconButton } from '@angular/material/button';
+import { MatCardContent } from '@angular/material/card';
+import { MatIcon } from '@angular/material/icon';
+
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   standalone: true,
-  imports: [],
+  imports: [
+    MatToolbarModule,
+    MatIcon,
+    MatCardContent,
+    RouterOutlet,
+    MatIconButton,
+  ],
   templateUrl: './main.component.html',
-  styleUrl: './main.component.scss'
+  styleUrl: './main.component.scss',
 })
-export class MainComponent {
-
-}
+export class MainComponent {}
