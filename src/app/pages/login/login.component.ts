195f51273791759d715ed7da54997743f6ae0129
Eduardo-Torres6720 <eduardotorres6720@gmail.com>
2024-08-06 16:00:17 -0300

    feat: first version layout login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,48 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardTitle,
+} from '@angular/material/card';
+import {
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+} from '@angular/forms';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCardContent,
+    MatCard,
+    MatCardTitle,
+    MatFormField,
+    MatLabel,
+    MatError,
+    MatButtonModule,
+    MatCardActions,
+    MatInput,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  fb = inject(FormBuilder);
 
+  form = this.fb.group({
+    emailFormControl: ['', [Validators.required, Validators.email]],
+    senhaFormControl: ['', [Validators.required, Validators.minLength(3)]],
+  });
+
+  logar() {
+    console.log('logado');
+  }
 }
