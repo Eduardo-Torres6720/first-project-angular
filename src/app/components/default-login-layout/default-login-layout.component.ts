1631171a978f042a64e84ccdf6addc0c98785a0e
Eduardo-Torres6720 <eduardotorres6720@gmail.com>
2024-08-06 17:50:39 -0300

    feat: add default-login-layout

---
+++ src/app/components/default-login-layout/default-login-layout.component.ts
@@ -0,0 +1,51 @@
+import { Component, EventEmitter, inject, Input, Output } from '@angular/core';
+import {
+  MatCard,
+  MatCardActions,
+  MatCardContent,
+  MatCardTitle,
+} from '@angular/material/card';
+import {
+  Validators,
+  FormsModule,
+  ReactiveFormsModule,
+  FormBuilder,
+} from '@angular/forms';
+import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+@Component({
+  selector: 'app-default-login-layout',
+  standalone: true,
+  imports: [
+    MatCardContent,
+    MatCard,
+    MatCardTitle,
+    MatFormField,
+    MatLabel,
+    MatError,
+    MatButtonModule,
+    MatCardActions,
+    MatInput,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  templateUrl: './default-login-layout.component.html',
+  styleUrl: './default-login-layout.component.scss',
+})
+export class DefaultLoginLayoutComponent {
+  @Input() title: string = '';
+  @Input() textButton: string = '';
+  @Output('submit') onSubmit = new EventEmitter();
+
+  submit() {
+    this.onSubmit.emit();
+  }
+
+  fb = inject(FormBuilder);
+
+  form = this.fb.group({
+    emailFormControl: ['', [Validators.required, Validators.email]],
+    senhaFormControl: ['', [Validators.required, Validators.minLength(3)]],
+  });
+}
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,47 +1,14 @@
-import { Component, inject } from '@angular/core';
-import {
-  MatCard,
-  MatCardActions,
-  MatCardContent,
-  MatCardTitle,
-} from '@angular/material/card';
-import {
-  Validators,
-  FormsModule,
-  ReactiveFormsModule,
-  FormBuilder,
-} from '@angular/forms';
-import { MatError, MatFormField, MatLabel } from '@angular/material/form-field';
-import { MatInput } from '@angular/material/input';
-import { MatButtonModule } from '@angular/material/button';
+import { Component } from '@angular/core';
+import { DefaultLoginLayoutComponent } from '../../components/default-login-layout/default-login-layout.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [
-    MatCardContent,
-    MatCard,
-    MatCardTitle,
-    MatFormField,
-    MatLabel,
-    MatError,
-    MatButtonModule,
-    MatCardActions,
-    MatInput,
-    FormsModule,
-    ReactiveFormsModule,
-  ],
+  imports: [DefaultLoginLayoutComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  fb = inject(FormBuilder);
-
-  form = this.fb.group({
-    emailFormControl: ['', [Validators.required, Validators.email]],
-    senhaFormControl: ['', [Validators.required, Validators.minLength(3)]],
-  });
-
   logar() {
     console.log('logado');
   }
